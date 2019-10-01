1c61d18f3e3852d2b2525419008a105dc2f66f18
Alicia Escarda <escardaaliciamae@gmail.com>
2019-10-01 23:58:55 +0800

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,45 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-sass.compiler = require('node-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const browserSync = require('browser-sync').create();
 
-function sass() {
-  return gulp.src('./scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+function sassTask() {
+	return gulp.src('./scss/*.scss') // any folder that has scss
+		.pipe(sass().on('error', sass.logError)) // pass to sass compiler
+		.pipe(gulp.dest('./css')) // destination
+		.pipe(browserSync.stream()) // stream changes to browser
 }
- 
-function js(){
+
+function jsTask() {
   return gulp.src('js/*.js'),
         uglify(),
         gulp.dest('dist');
 }
 
 function watch() {
-	gulp.watch(['./scss/*.scss','js/*.js'],
-	gulp.parallel(sass,js));
-	
-}
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+	gulp.watch('./scss/*.scss', sassTask); // reload all scsss file and call the function sassTask
+	gulp.watch('./*.html').on('change', browserSync.reload); // reload the whole page 
+	gulp.watch('js/*.js', jsTask) // reload all js file and call the function jsTask
 
-exports.default = gulp.series(
-	gulp.parallel(sass,js),
-	watch
-);
+}
+ 
+
+// function watchTask() {
+// 	gulp.watch(['./scss/*.scss','js/*.js'],
+// 	gulp.parallel(sassTask,jsTask));
+// }
+
+exports.watch = watch;
+exports.sassTask = sassTask;
+exports.jsTask = jsTask;
+// exports.default = gulp.series(
+// 	gulp.parallel(sassTask,jsTask),
+// 	watchTask
+// );
 
 
