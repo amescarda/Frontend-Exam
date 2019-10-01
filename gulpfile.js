24e2452629a0feb8d2962113e2d65dbc84b98dd0
Alicia Escarda <escardaaliciamae@gmail.com>
2019-10-02 00:49:30 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,20 @@ const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 const browserSync = require('browser-sync').create();
 
+const  file = {
+	sassPath : './scss/*.scss',
+	jsPath : './js/*.js',
+}
+
 function sassTask() {
-	return gulp.src('./scss/*.scss') // any folder that has scss
+	return gulp.src(sassPath) // any folder that has scss
 		.pipe(sass().on('error', sass.logError)) // pass to sass compiler
-		.pipe(gulp.dest('./css')) // destination
+		.pipe(gulp.dest('dist')) // destination
 		.pipe(browserSync.stream()) // stream changes to browser
 }
 
 function jsTask() {
-	return gulp.src('/js/*.js')
+	return gulp.src(jsPath)
         .pipe(uglify())
 		.pipe(dest('dist'))
 }
@@ -34,7 +39,7 @@ function watchTask() {
 			baseDir: './'
 		}
 	});
-	gulp.watch(['./scss/*.scss','js/*.js'],
+	gulp.watch([sassPath,jsPath],
 	gulp.parallel(sassTask,jsTask));
 }
 
