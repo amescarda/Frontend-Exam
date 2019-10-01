f9ae902b7d2cc3aa543e44aad76756936df7ae65
Alicia Escarda <escardaaliciamae@gmail.com>
2019-10-01 22:50:01 +0800

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,29 @@
-var gulp = require('gulp')
-var sass = require('gulp-sass')
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
-sass.compiler = require('node-sass')
+sass.compiler = require('node-sass');
 
 function sass() {
   return gulp.src('./scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
 }
  
 function js(){
   return gulp.src('js/*.js'),
         uglify(),
-        gulp.dest('dist')
+        gulp.dest('dist');
 }
 
 function watch() {
 	gulp.watch(['./scss/*.scss','js/*.js'],
-	gulp.parallel(sass,js))
+	gulp.parallel(sass,js));
+	
 }
 
-exports.default = gulp.series(gulp.parallel(js,sass),watch) 
+exports.default = gulp.series(
+	gulp.parallel(sass,js),
+	watch
+);
 
 
