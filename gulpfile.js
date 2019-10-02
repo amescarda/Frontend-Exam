2d1b3343338f13408988aa967c6581ee1d4d02f6
Alicia Escarda <escardaaliciamae@gmail.com>
2019-10-02 15:58:19 +0800

    added bower jquery

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,50 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
+const mainBowerFiles = require('main-bower-files');
 const browserSync = require('browser-sync').create();
 
 const  file = {
 	sassPath : './scss/*.scss',
 	jsPath : './js/*.js',
+	htmlPath : './*.html',
 }
 
 function sassTask() {
-	return gulp.src(sassPath) // any folder that has scss
+	return gulp.src(file.sassPath) // any folder that has scss
 		.pipe(sass().on('error', sass.logError)) // pass to sass compiler
 		.pipe(gulp.dest('dist')) // destination
 		.pipe(browserSync.stream()) // stream changes to browser
 }
 
 function jsTask() {
-	return gulp.src(jsPath)
+	return gulp.src(file.jsPath)
         .pipe(uglify())
-		.pipe(dest('dist'))
+		.pipe(gulp.dest('dist'))
+		.pipe(browserSync.stream()) // stream changes to browser
 }
 
-// function watch() {
-// 	browserSync.init({
-// 		server: {
-// 			baseDir: './'
-// 		}
-// 	});
-// 	gulp.watch('./scss/*.scss', sassTask) // reload all scsss file and call the function sassTask
-// 	gulp.watch('./*.html').on('change', browserSync.reload) // reload the whole page 
-// 	gulp.watch('./js/*.js', jsTask) // reload all js file and call the function jsTask
+// function bowerTask() {
+// 	return gulp.src(mainBowerFiles() , { base: '/bower_components/'  })
+//         .pipe(uglify())
+// 		.pipe(gulp.dest('dist'))
+// 		.pipe(browserSync.stream()) // stream changes to browser
 // }
- 
 
-function watchTask() {
+function watch() {
 	browserSync.init({
 		server: {
 			baseDir: './'
 		}
 	});
-	gulp.watch([sassPath,jsPath],
-	gulp.parallel(sassTask,jsTask));
+	gulp.watch(file.sassPath, sassTask) // reload all scsss file and call the function sassTask
+	gulp.watch(file.htmlPath).on('change', browserSync.reload) // reload the whole page 
+	gulp.watch(file.jsPath, jsTask) // reload all js file and call the function jsTask
 }
-
-// exports.watch = watch;
-// exports.sassTask = sassTask;
-// exports.jsTask = jsTask;
+ 
 exports.default = gulp.series(
 	gulp.parallel(sassTask,jsTask),
-	watchTask
+	watch
 )
 
 
--- js/app.js
+++ js/app.js
@@ -1,1 +1,3 @@
-console.log('hello test')
+$(document).ready(function() {
+	console.log('meow')
+})
